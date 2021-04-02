429bf7945268203bd16fb1808aa2fb4fa9735d61
Gabriel J. Reinert <gabriel.j.reinert@gmail.com>
2021-04-02 12:02:15 -0300

    Changed index to be add jobs dinamically and created functions to handle it in the routes

--- src/routes.js
+++ src/routes.js
@@ -10,11 +10,49 @@ const profile = {
 	"monthly-budget": 3000,
 	"days-per-week": 5,
 	"hours-per-day": 5,
-	"vacations-per-year": 4
+	"vacations-per-year": 4,
+	"value-hour": 75
 }
 
+
+const jobs = [{
+	"id": 1,
+	"name": "Pizzaria",
+	"daily-hours": 5,
+	"total-hours": 100,
+	"createdAt": Date.now()
+}]
+
+function remainingDays(job) {
+		const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
+		
+		const createdDate = new Date(job.createdAt)
+		const dueDay = createdDate.getDate() + Number(remainingDays)
+		const dueDate = createdDate.setDate(dueDay)
+
+		const timeDiffInMs = dueDate - Date.now()
+		const dayInMs =  1000 * 60 * 60 * 24
+		const dayDiff = Math.floor(timeDiffInMs/ dayInMs)
+
+		return dayDiff 
+}
+
+//GET requisitions
 routes.get("/", (req, res) => {
-	return res.render(views + "index")
+	const updatedJobs = jobs.map((job) => {
+		const remaining = remainingDays(job)
+		const status = remaining <= 0 ? 'done' : 'progress'
+
+		return {
+			...job,
+			remaining,
+			status,
+			"budget": profile['value-hour'] * job['total-hours']
+		}
+
+	})
+
+	return res.render(views + "index", { jobs: updatedJobs })
 })
 
 routes.get("/job", (req, res) => {
@@ -27,6 +65,20 @@ routes.get("/job/edit", (req, res) => {
 
 routes.get("/profile", (req, res) => {
 	return res.render(views + "profile", { profile })
+})
+
+//POST requisitions
+routes.post("/job", (req, res) => {
+	const lastId = jobs[jobs.length - 1]?.id || 0;
+
+	jobs.push({
+		"id": lastId + 1,
+		"name": req.body.name,
+		"daily-hours": req.body["daily-hours"],
+		"total-hours": req.body["total-hours"],
+		"createdAt": Date.now()
+		})
+	return res.redirect("/")
 })
 
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes.js")
+const PORT = 3000
 
 server.set("view engine", "ejs")
 
 server.use(express.static("public"))
 
+server.use(express.urlencoded({extended: true}))
+
 server.use(routes)
 
-server.listen(3000, () => console.log("Server on"))
+server.listen(PORT, () => console.log("Server on"))
 
